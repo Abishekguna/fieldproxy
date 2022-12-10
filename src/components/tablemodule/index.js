fc74cc9517b62eccc985d8ee9a0bab77cadad0bb
Abishekguna <abishekg77g@gmail.com>
2022-12-10 19:50:03 +0530

    second

--- src/components/tablemodule/index.js
+++ src/components/tablemodule/index.js
@@ -1,7 +1,162 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
 export const TableModule = () => {
+  // getting value from redux by useselector
+  //sending value by dispatch to redux
+  const dispatch = useDispatch();
+  const storeData = useSelector((store) => store.tableData);
+  const [edit, setEdit] = useState(false);
+  //name
+  const [name, setName] = useState("");
+  //   //date
+  const [date, setDate] = useState("");
+  //   //age
+  const [age, setAge] = useState("");
+  
+  const [indexUpdate, setIndexUpdate] = useState(0);
+
+  let navigate = useNavigate();
+
+  function onEdit(index, dat) {
+    setEdit(true);
+
+    setName(dat.name);
+    setDate(dat.date);
+    setAge(dat.age);
+
+    setIndexUpdate(index);
+  }
+  function reDirect2() {
+    navigate("/view");
+  }
+  
+  function onAdd() {
+    let updatedValue = [...storeData];
+
+    updatedValue[indexUpdate] = {
+      name: name,
+      date: date,
+      age: age,
+
+    };
+    dispatch({ type: "table", payload: updatedValue });
+    setEdit(false);
+  }
+  const onDelete = (index) => {
+    let updatedValue = [...storeData];
+    updatedValue.splice(index, 1);
+    dispatch({ type: "delete", payload: updatedValue });
+  };
   return (
-    <div>TableModule</div>
-  )
-}
+    <div>
+     
+      <div className="container p-5">
+        <h1 className="text-center">Table Module</h1>
+        <div className="float-end">
+          {" "}
+          <button className="bg-dark text-light" onClick={reDirect2}>
+            View Table
+          </button>
+        </div>
+        <table className="table table-border text-center    w-100">
+          <tr className="bg-dark text-light">
+            <th>Name</th>
+            <th>Date of Birth</th>
+            <th>Age</th>
+          
+            <th>Updation</th>
+            <th>Remove</th>
+          </tr>
+          {console.log(storeData, "tabledatav")}
+
+          {storeData.map((dat, index) => {
+            return (
+              <tbody>
+                <tr>
+                  <td key={index} index={index}>
+                    {dat.name}
+                  </td>
+
+                  <td>{dat.date}</td>
+                  <td>{dat.age}</td>
+     
+                  <td>
+                    <button onClick={() => onEdit(index, dat)}>edit</button>
+                  </td>
+                  <td>
+                    {" "}
+                    <button onClick={() => onDelete(index, dat)}>Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
+      <div>
+      {edit ? (
+        <div>
+          <div className="container text-center ">
+            <h1 className="text-center">Edit Module</h1>
+            <table className="table container  text-light w-75">
+              <thead className="bg-dark text-light">
+                <tr>
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th>Age</th>
+              
+                  <th>Update</th>
+                 
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input
+                      name="name"
+                      placeholder="name"
+                      value={name}
+                      type="text"
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name="date"
+                      value={date}
+                      type="date"
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                  </td>
+                  <td>
+                    {" "}
+                    <input
+                      name="age"
+                      value={age}
+                      placeholder="age"
+                      onChange={(e) => setAge(e.target.value)}
+                      type="number"
+                    />
+                  </td>
+                
+
+                  <td>
+                    {" "}
+                    <button onClick={onAdd}>Submit</button>{" "}
+                  </td>
+                 
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}{" "}
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './router';
+import { StrictMode } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { App} from "./router";
+import Reducer from "./reduxstore";
+
+const store = createStore(Reducer);
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  
-    <App/>
-
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
-
 
